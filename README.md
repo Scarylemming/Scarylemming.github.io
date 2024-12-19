2164859ef1a4257999091473048bb9264f6f6f79
Juraj Rosinsky <juraj.rosinsky@gmail.com>
2024-12-19 12:03:44 +0100

    Remove the previous website

--- README.md
+++
@@ -1,1 +0,0 @@
-My first try to make a website. For now, there is a try to make a counter for Jass, a Swiss card game.
--- load-banner.js
+++
@@ -1,4 +0,0 @@
-// Load and insert banner HTML
-fetch('banner.html')
-    .then(response => response.text())
-    .then(data => document.querySelector('.banner-container').innerHTML = data);
